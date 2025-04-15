f52fbed660339af541aa9b671c914f70701dc753
nathen-win <rpriyo.uic.edu>
2025-04-15 15:55:29 -0500

    Added custom hook useHttp

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,22 @@
-import { useEffect, useState } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
-export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
-    
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
-    
-            if (!response.ok) {
-                // ...
-            }
-    
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
+const requestConfig = {};
 
-        fetchMeals();
-    }, []);
+export default function Meals() {
+    const {
+        data: loadedMeals, 
+        isLoading, 
+        error,
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+    if (isLoading) {
+        return <p>Fetching meals...</p>;
+    }
+
+    // if (!data) {
+    //     return <p>No meals found.</p>
+    // }
     
 
     return (
