f46b6f39b0766be6114112712a6344b0afc683af
nathen-win <rpriyo.uic.edu>
2025-04-15 00:16:34 -0500

    Added Checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -18,8 +18,16 @@ export default function Cart() {
         userProgressCtx.hideCart();
     }
 
+    function handleOpenCheckout() {
+        userProgressCtx.showCheckout();
+    }
+
     return (
-        <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+        <Modal 
+            className="cart" 
+            open={userProgressCtx.progress === 'cart'} 
+            onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}
+        >
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map(item => 
@@ -36,7 +44,7 @@ export default function Cart() {
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
                 <Button textOnly onClick={handleCloseCart}>Close</Button>
-                <Button onClick={handleCloseCart}>Go to Checkout</Button>
+                {cartCtx.items.length > 0 && <Button onClick={handleOpenCheckout}>Go to Checkout</Button>}
             </p>
         </Modal>
     )
